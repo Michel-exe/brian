259e7f726e1ac00efe7d4c86f98a10b24446d37c
Michel-exe <michl521521@gmail.com>
2023-01-10 10:27:56 -0600

    Version sin Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import "./style/animaciones.css"
+import "./style/header.css"
+import "./style/presentacion.css"
 import "./style/style.css"
-import {Header2 as Header} from "./components/Header";
-import { Presentacion as Presentacion } from "./components/Presentacion";
+import {Header3 as Header} from "./components/Header";
+import { Presentacion } from "./components/Presentacion";
 import { useState } from "react";
 import { Habilities } from "./components/Habilities";
 import { Works } from "./components/Works";
+import { Curriculumn } from "./components/Curriculumn";
 
 function App() {
    let [tr,settr] = useState("0");
@@ -14,6 +18,7 @@ function App() {
          <Presentacion settr={settr} setCla={setCla} ></Presentacion>
          <Habilities settr={settr} setCla={setCla} ></Habilities>
          <Works settr={settr} setCla={setCla} ></Works>
+         <Curriculumn settr={settr} setCla={setCla} ></Curriculumn>
       </div>
    );
 }
--- src/components/Habilities.jsx
+++ src/components/Habilities.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import {FaReact,FaCss3} from 'react-icons/fa'
 import {IoLogoJavascript,IoLogoSass,IoLogoNodejs} from 'react-icons/io'
 import {ImHtmlFive} from 'react-icons/im'
 import {BiGitBranch,BiCodeCurly} from 'react-icons/bi'
 import {HiArrowDown, HiArrowUp} from 'react-icons/hi'
-import {FaJava} from 'react-icons/fa'
+import {FaJava,FaAssistiveListeningSystems} from 'react-icons/fa'
 import {SiTypescript} from 'react-icons/si'
 import {GrMysql} from 'react-icons/gr'
 import {DiPhp} from 'react-icons/di'
+import {CgFileDocument} from 'react-icons/cg'
+import {RxSpeakerLoud} from 'react-icons/rx'
 
 export const Habilities = ({settr,setCla}) => {
     const com = (tit,col,com) =>{
         return (
             <section 
-                style={{background: col,background:`linear-gradient(-180deg, #00000000 78%, #000 78%)`}}
+                style={{background:`linear-gradient(-180deg, #00000000 78%, #000 78%)`}}
             >
                 <div className="rgb"></div>
                 <span>
@@ -30,7 +32,7 @@ export const Habilities = ({settr,setCla}) => {
         <div className="cont">
             <HiArrowUp onClick={()=>{settr('-300');setCla('1')}} className='dir arriba'></HiArrowUp>
             <HiArrowDown onClick={()=>{settr('-600');setCla('3')}} className='dir abajo'></HiArrowDown>
-            <h2>Habilities</h2>
+            <h2 className='txt-tit'>Habilities</h2>
             <nav>
                 {com("REACT","#61dbfb",<FaReact />)}
                 {com("JAVASCRIPT","#fff700",<IoLogoJavascript />)}
@@ -40,10 +42,13 @@ export const Habilities = ({settr,setCla}) => {
                 {com("PHP","#cd6799",<DiPhp />)}
                 {com("MySQL","#cd6799",<GrMysql />)}
                 {com("GIT","#ffffff",<BiGitBranch />)}
-                {/* {com("JAVA","#ffffff",<FaJava />)}
+                {com("ESCUCHAR EN INGLES","#ffffff",<FaAssistiveListeningSystems />)}
+                {com("HABLAR EN INGLES","#ffffff",<RxSpeakerLoud />)}
+                {com("ESCRIBIR EN INGLES","#ffffff",<CgFileDocument />)}
+                {com("JAVA","#ffffff",<FaJava />)}
                 {com("C","#ffffff",<BiCodeCurly />)}
                 {com("Node","#3c873a",<IoLogoNodejs />)}
-                {com("TypeScript","#007acc",<SiTypescript />)} */}
+                {com("TypeScript","#007acc",<SiTypescript />)}
             </nav>
         </div>
     </div>
